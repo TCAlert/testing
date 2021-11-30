5ad2f92b035b92d9e7dbf84cb15cbe540f4414e8
TCAlert <59183886+TCAlert@users.noreply.github.com>
2021-11-30 15:47:27 -0500

    Update dynamiccmap.py
    Added detail, removed import

--- dynamiccmap.py
+++ dynamiccmap.py
@@ -1,20 +1,11 @@
-from netCDF4 import Dataset      # Read / Write NetCDF4 files
-import matplotlib.pyplot as plt  # Plotting library
-from cpt_convert import loadCPT # Import the CPT convert function
-import cartopy, cartopy.crs as ccrs  # Plot maps
-import numpy.ma as ma
 import numpy as np
-import pandas as pd
-import xarray as xr
-import metpy 
-import windclimo as wc
 from datetime import datetime 
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-import goesRequest2 as goes2
 import cgfs as gfs
 import bdeck as bdeck 
 
+# Retrieve relevant GFS Data
 def getGFSData(storm):
     requests = ['tmpprs', 'tmptrop']
     t = datetime.utcnow()
@@ -29,6 +20,7 @@ def getGFSData(storm):
 
     return tmp500, tmptro, lat, lon, time
 
+# Create dynamic colormap off of GFS data
 def dynamicCMAP(storm):
     tmp500, tmptro, lat, lon, time = getGFSData(storm)
 
@@ -44,21 +36,4 @@ def dynamicCMAP(storm):
     newcmp = ListedColormap(newcolors, name='temp')
     return newcmp, tmp500, tmptro, lat, lon, time
 
-#tmp500, tmptro, lat, lon = getGFSData('al94')
-
-#ax = plt.axes(projection=ccrs.Geostationary(central_longitude=-75.0, satellite_height=35786023.0))
-#img_extent = (-5434894.67527,5434894.67527,-5434894.67527,5434894.67527)
- 
-## Add coastlines, borders and gridlines
-#ax.coastlines(resolution='10m', color='black', linewidth=0.8)
-#ax.add_feature(cartopy.feature.BORDERS, edgecolor='black', linewidth=0.5) 
-
-#data, center, info, time = goes2.getData('16', '13')
-#plt.imshow(data - 273, origin = 'upper', extent = img_extent, vmin = -110, vmax = 40, cmap = dynamicCMAP('al94'))#, transform = ccrs.Geostationary(central_longitude = center, satellite_height=35786023.0))
-#plt.colorbar(orientation = 'vertical', aspect = 50, pad = .02)    
-
-#plt.title(f'Dynamic Colortable Test\n5x5 Box At {lat}N, {360-lon}W' , fontweight='bold', fontsize=10, loc='left')
-#plt.title(f'500mb Temp:        {tmp500}C\nTropopause Temp:  {tmptro}C\nTCAlert', fontsize=10, loc='right')
-
-#plt.show()
-
+# Largely experimental, write up here: https://cdn.discordapp.com/attachments/843292360275394610/915342804715208744/Regarding_the_Usage_of_a_Dynamic_Colormap_for_Deep_Convection.pdf
