62bbc45a131fa8882528d870089d0b4dd45c2a15
TCAlert <59183886+TCAlert@users.noreply.github.com>
2021-12-03 00:37:49 -0500

    Update msfc.py
    Added usage instructions

--- msfc.py
+++ msfc.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import urllib.request
 import imageio
 
-usage = '```$msfc [satellite] [bands 2, 5, 7, 8, 13, or 14] [latitude] [longitude]```'
+# sample usage
+# build('east', 2, 45, -45)
 
 def build(satt, band, lat, lon):
     if band == '2' or band == '5':
@@ -31,8 +32,7 @@ def build(satt, band, lat, lon):
 
     with imageio.get_writer(r"C:\Users\Jariwala\Downloads\msfcloop.gif", mode='I', duration = 0.1) as writer:
         for x in range(len(images) - 1):
-            writer.append_data(imageio.imread((r"C:\Users\Jariwala\Downloads\msfcloops\imagenumber" + str(x + 1) + ".png").format(i = x)))
+            writer.append_data(imageio.imread((r"C:\Users\[Username]\Downloads\msfcloops\imagenumber" + str(x + 1) + ".png").format(i = x)))
         for y in range(0, 9):
-            writer.append_data(imageio.imread((r"C:\Users\Jariwala\Downloads\msfcloops\imagenumber" + str(x + 1) + ".png").format(i = x)))
+            writer.append_data(imageio.imread((r"C:\Users\[Username]\Downloads\msfcloops\imagenumber" + str(x + 1) + ".png").format(i = x)))
 
-#build('west', 9, 13.2, -136.5)
