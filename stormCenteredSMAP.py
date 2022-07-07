cbea21ae70345dd0a6830a4ee2c433bd4b5f2383
TCAlert <59183886+TCAlert@users.noreply.github.com>
2022-07-06 21:20:46 -0400

    Update stormCenteredSMAP.py
    Fixed a bug where the time was not displaying correctly.

--- stormCenteredSMAP.py
+++ stormCenteredSMAP.py
@@ -31,8 +31,6 @@ def smap(storm, ascending):
     
     # Retrieve needed data from file
     minu = data['minute'].sel(node = node)
-    minu = minu.sel(lat = lat, lon = lon, method = 'nearest')
-    time = f'{str(int(minu / 60)).zfill(2)}{str(int(minu % 60)).zfill(2)}z'
     data = data['wind'].sel(node = node)
 
     # Mask unnecessary information to speed up processing
@@ -42,10 +40,17 @@ def smap(storm, ascending):
     
     # Convert data to knots and retrieve maximum value
     data = data * 1.944
-    given = np.nanmax(data)
+    given = data.where(data == data.max(), drop = True).squeeze()
     
     # Calculate maximum winds using the formula in the usage instructions
     calc = ((given * 362.644732816453) + 2913.62505913216) / 380.88384339523
+
+    try:
+        time = minu.sel(lat = lat, lon = lon, method = 'nearest')
+        time = f'{str(int(time / 60)).zfill(2)}{str(int(time % 60)).zfill(2)}z'
+    except:
+        time = minu.sel(lat = given.lat, lon = given.lon, method = 'nearest')
+        time = f'{str(int(time / 60)).zfill(2)}{str(int(time % 60)).zfill(2)}z'
 
     # Create plot
     plt.figure(figsize=(20,12))
@@ -60,7 +65,7 @@ def smap(storm, ascending):
     c = plt.contour(data.lon, data.lat, data.values, origin='lower', levels = [34, 64, 96, 137], colors = 'black', linewidths = 2, transform=ccrs.PlateCarree(central_longitude=0))
     plt.contourf(data.lon, data.lat, data.values, origin='lower', levels = range(0, 160), cmap = cmap.wind(), transform=ccrs.PlateCarree(central_longitude=0))
     plt.clabel(c, inline=1, fontsize=10, fmt='%1.0f')
-    plt.title(f'SMAP Pass on {date} at {time} ({asc})\nCalculated Windspeed - {str(round(np.nanmax(calc), 2))} knots | Max Wind (10 min) - {str(round(given, 2))} knots' , fontweight='bold', fontsize=10, loc='left')
+    plt.title(f'SMAP Pass on {date} at {time} ({asc})\nCalculated Windspeed - {str(round(float(calc.values), 2))} knots | Max Wind (10 min) - {str(round(float(given.values), 2))} knots' , fontweight='bold', fontsize=10, loc='left')
     plt.title('TCAlert', fontsize=10, loc='right') 
     plt.colorbar(orientation = 'vertical', aspect = 50, pad = .02)
     plt.savefig(r"C:\Users\[redacted]\Downloads\stormSMAP.png", dpi = 250, bbox_inches = 'tight')
