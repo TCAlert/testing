4c1c9eedfde59727f574442713ef6a07ae3f5533
Deelan Jariwala <deelanjariwa@gmail.com>
2024-02-11 17:36:05 -0500

    Added retrieval of hourly data

--- psl.py
+++ psl.py
@@ -14,5 +14,22 @@ def createClimoMonthly(allYears, month, var, sigma):
         data.append(tempDataset.mean(['time']))
     return data 
 
+
+def getHourlyData(year, month, day, hour, var, level):
+    if str(level).lower() == 'surface' and var.lower() != 'slp':
+        link = f'http://psl.noaa.gov/thredds/dodsC/Datasets/ncep.reanalysis/surface/{var}.sig995.{year}.nc'
+    elif str(level).lower() == 'surface' and var.lower() == 'slp':
+        link = f'http://psl.noaa.gov/thredds/dodsC/Datasets/ncep.reanalysis/surface/{var}.{year}.nc'
+    else:
+        link = f'http://psl.noaa.gov/thredds/dodsC/Datasets/ncep.reanalysis/pressure/{var}.{year}.nc'
+    data = xr.open_dataset(link)
+    
+    try:
+        data = data[var].sel(time = np.datetime64(f'{year}-{str(month).zfill(2)}-{str(day).zfill(2)}T{str(hour).zfill(2)}'), level = level)
+    except:
+        data = data[var].sel(time = np.datetime64(f'{year}-{str(month).zfill(2)}-{str(day).zfill(2)}T{str(hour).zfill(2)}'))
+
+    return data
+
 #data = createClimoMonthly([1983], '01', ['omega', 'uwnd', 'chi'], [False, False, True])
 #print(data)
