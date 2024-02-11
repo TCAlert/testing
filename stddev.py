169c781a97757567b1798cf10d60c4a387ff9441
Deelan Jariwala <deelanjariwa@gmail.com>
2024-02-11 17:37:05 -0500

    StdDev --> QCOD?

--- stddev.py
+++ stddev.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import cartopy.feature as cfeature
 import cmaps as cmap
 import bdeck as bdeck 
+import cartopy.mpl.ticker as cticker
 
 # Move GEFS functions to own file? 
 # Suggest merger of GEFS and GFS code, make single clear function
@@ -74,22 +75,27 @@ def bounds(basin):
         bounds = [lon - 27.5, lon + 27.5, lat - 15, lat + 15]
     return bounds
 
-def map():
-    # legit the only reason why this exists is to clean shit up lol
-
-    fig = plt.figure(figsize=(20, 9))
+# Create a map using Cartopy
+def map(interval, labelsize):
+    fig = plt.figure(figsize=(18, 9))
 
     # Add the map and set the extent
     ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
-
-    # Add state boundaries to plot
-    ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth = 1)
-    ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth = 1)
-    ax.add_feature(cfeature.STATES.with_scale('50m'), linewidth = 1)
-    gl = ax.gridlines(crs=ccrs.PlateCarree(central_longitude=180), zorder = 9, draw_labels = True, linewidth = 0.5, color='white', alpha=0.5, linestyle='--', transform = ccrs.PlateCarree(central_longitude=180))
-    gl.xlabels_top = gl.ylabels_right = False 
+    ax.set_frame_on(False)
     
-    return ax
+    # Add state boundaries to plot
+    ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth = 0.5)
+    ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth = 0.5)
+    ax.add_feature(cfeature.STATES.with_scale('50m'), linewidth = 0.5)
+    ax.set_xticks(np.arange(-180, 181, interval), crs=ccrs.PlateCarree())
+    ax.set_yticks(np.arange(-90, 91, interval), crs=ccrs.PlateCarree())
+    ax.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+    ax.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+    ax.tick_params(axis='both', labelsize=labelsize, left = False, bottom = False)
+    ax.grid(linestyle = '--', alpha = 0.5, color = 'black', linewidth = 0.5, zorder = 9)
+
+    return ax 
+
 
 def mean(dataset):
     data = []
@@ -111,8 +117,8 @@ def std(dataset, average):
     return stddev
 
 def run(basin):
-    requests = ['hgtprs', 'ugrdprs', 'vgrdprs']
-    hour = 15
+    requests = ['hgtprs']
+    hour = 28
     bound = bounds(basin)
     height = 500
 
@@ -122,29 +128,30 @@ def run(basin):
         dat[x] = dat[x].sel(lon = slice(bound[0], bound[1]), lat = slice(bound[2], bound[3]), lev = height) 
     
     dat[0] = dat[0] / 10
-    dat[1] = mean(dat[1] * 1.944)[::8, ::8]
-    dat[2] = mean(dat[2] * 1.944)[::8, ::8]
-
+    q1 = np.percentile(dat[0], 25, axis = 0)
+    q3 = np.percentile(dat[0], 75, axis = 0)
     avg = mean(dat[0])
     dev = std(dat[0], avg)
+    qcod = (q3 - q1)**2 / (q3 + q1)
+    print(qcod)
 
-    ax = map()
+    ax = map(4, 8)
     ax.set_extent(bound, crs = ccrs.PlateCarree())
 
-    plt.contourf(dev.lon, dev.lat, dev.values, levels = np.arange(0, 10, 0.025), cmap = cmap.stddev(), extend = 'both')#, transform = ccrs.PlateCarree(central_longitude = 180))
+    plt.contourf(dev.lon, dev.lat, qcod, levels = np.arange(0, 1, 0.001), cmap = cmap.probs(), extend = 'both')#, transform = ccrs.PlateCarree(central_longitude = 0))
     plt.colorbar(orientation = 'vertical', aspect = 50, pad = .02)
-    c = plt.contour(avg.lon, avg.lat, avg.values, levels = np.arange(500, 600, 2.5), colors = 'black')
+    c = plt.contour(avg.lon, avg.lat, avg.values, levels = np.arange(500, 600, 4), linewidths = 1, colors = '#1a1a1a')
     plt.clabel(c, fontsize = 10, inline = 1, fmt = '%1.0f')
-    plt.barbs(dat[1].lon, dat[2].lat, dat[1].values, dat[2].values, length = 6)
+
     mdate = f'{mdate[:4]}-{mdate[4:6]}-{mdate[6:8]}'
     time = (str(dev.time.values)).split('T')
     time = f'{time[0]} at {(time[1][:5])}z'
 
-    plt.title(f'0.5\u00b0 GEFS Mean 500mb Height Contours and Standard Deviation (dam), Wind Barbs (knots)\nInitialization: {mdate} at {init_hour}:00z', fontweight='bold', fontsize=10, loc='left')
+    plt.title(f'0.5\u00b0 GEFS Mean 500mb Height Contours (dam) and IQR * QCOD \nInitialization: {mdate} at {init_hour}:00z', fontweight='bold', fontsize=10, loc='left')
     plt.title(f'Forecast Hour: {str(hour * 6)} ({time})', fontsize = 10, loc = 'center')
-    plt.title('TCAlert\nData from NOMADS', fontsize=10, loc='right') 
-    #plt.savefig(r"C:\Users\Username\Downloads\stddevplot.png", dpi = 150, bbox_inches = 'tight')
+    plt.title('Deelan Jariwala\nData from NOMADS', fontsize=10, loc='right') 
+    plt.savefig(r"C:\Users\deela\Downloads\stddevplot.png", dpi = 150, bbox_inches = 'tight')
     #print("Saving complete")
     plt.show()
     plt.close() 
-#run('us')
+run('natl')
