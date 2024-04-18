938291c2e310fd313c098e98bcb994e966b5e639
Deelan Jariwala <deelanjariwa@gmail.com>
2024-04-18 10:39:12 -0400

    New MRMS plot, retrieving data from AWS

--- mrms.py
+++ mrms.py
@@ -1,59 +1,97 @@
-import matplotlib.pyplot as plt 
+import xarray as xr 
+import matplotlib.pyplot as plt
 import cartopy, cartopy.crs as ccrs
-import cmaps as cmap 
-import xarray as xr
+import cartopy.mpl.ticker as cticker
+import cartopy.feature as cfeature
+import numpy as np 
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 from datetime import datetime
-import bdeck as bdeck 
+import cmaps as cmap 
+import file 
 
-usage = '```$mrms [storm (best track ID)/latitude] [longitude]```'
+# Function that retrieves Himawari-9 tiles of the requested band and melds them together
+# Data is returned at full resolution, regardless of band
+bucket = 'noaa-mrms-pds'
+product_name = 'MergedBaseReflectivity_00.50'
+def getData():
+    dataset = []
+    date = datetime.utcnow()
+    time = str(date.hour).zfill(2) + (str(date.minute))[0] + '0'
+    year = date.year
+    month = date.month
+    day = date.day
+    
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    prefix = f'CONUS/{product_name}/{year}{month:02.0f}{day:02.0f}/'
+    kwargs = {'Bucket': bucket,
+                'Prefix': prefix}
+
+    resp = s3_client.list_objects_v2(**kwargs)
+    files = []
+    for x in range(len(resp['Contents'])):
+        key = resp['Contents'][x]['Key']
+        if key.startswith(prefix):
+            files.append(key)
+    
+    f = files[-1]
+    print(f'Success! File Name: {f}')
+    
+    s3_client.download_file(bucket, f, r"C:\Users\deela\Downloads\mrms.grib2.gz")  
+
+    file.getGZ(r"C:\Users\deela\Downloads\mrms.grib2.gz", r"C:\Users\deela\Downloads\mrms.grib2")
+    data = xr.open_dataset(r"C:\Users\deela\Downloads\mrms.grib2", engine="cfgrib")
+
+    return data
+
+def map(interval, labelsize):
+    fig = plt.figure(figsize=(18, 9))
+
+    # Add the map and set the extent
+    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+    ax.set_frame_on(False)
+    
+    # Add state boundaries to plot
+    ax.add_feature(cfeature.COASTLINE.with_scale('10m'), edgecolor = 'white', linewidth = 0.5)
+    ax.add_feature(cfeature.BORDERS.with_scale('10m'), edgecolor = 'white', linewidth = 0.5)
+    ax.add_feature(cfeature.STATES.with_scale('10m'), edgecolor = 'white', linewidth = 0.5)
+    ax.set_xticks(np.arange(-180, 181, interval), crs=ccrs.PlateCarree())
+    ax.set_yticks(np.arange(-90, 91, interval), crs=ccrs.PlateCarree())
+    ax.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+    ax.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+    ax.tick_params(axis='both', labelsize=labelsize, left = False, bottom = False)
+    ax.grid(linestyle = '--', alpha = 0.5, color = 'grey', linewidth = 0.5, zorder = 9)
+
+    return ax 
 
 def plot(lat, lon = 0):
-    link = f'https://thredds.aos.wisc.edu/thredds/dodsC/grib/NCEP/MRMS/BaseRef/MRMS_BaseReflectivity_{datetime.utcnow().year}{str(datetime.utcnow().month).zfill(2)}{str(datetime.utcnow().day).zfill(2)}_{str(datetime.utcnow().hour).zfill(2)}00.grib2'
-    data = xr.open_dataset(link)
-
-    mrms = data['MergedBaseReflectivityQC_altitude_above_msl'].sel(time = data['time'][-1], altitude_above_msl = data['altitude_above_msl'][0])
-
-    plt.figure(figsize=(18, 9))
-    
-    #print('Plotting...')
-
-    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
-
     try:
         lat = float(lat)
         lon = float(lon)
         title = f'Centered at {lat}N, {abs(lon)}W'
-        lon += 360
-        extent = [lon - 1, lon + 1, lat - 1, lat + 1]
+        extent = [lon - 2.5, lon + 2.5, lat - 2.5, lat + 2.5]
     except:
         storm = lat
         lat, lon = bdeck.latlon(storm)
         title = storm.upper()
         extent = [lon - 2.5, lon + 2.5, lat - 2.5, lat + 2.5]
 
-    mrms = mrms.where((mrms.lon > extent[0]) & (mrms.lon < extent[1]) & (mrms.lat > extent[2]) & (mrms.lat < extent[3]), drop = True)
+    labelsize = 8
 
-    # Add coastlines, borders and gridlines
-    ax.coastlines(resolution='10m', color='white', linewidth=0.8)
-    ax.add_feature(cartopy.feature.BORDERS, edgecolor='white', linewidth=0.5) 
-    ax.add_feature(cartopy.feature.STATES.with_scale('10m'), edgecolor='white', linewidth=0.5) 
-    ax.add_feature(cartopy.feature.LAND, facecolor='#242424', linewidth=0.5) 
-    ax.add_feature(cartopy.feature.OCEAN, facecolor='#242424', linewidth=0.5) 
-    gl = ax.gridlines(crs=ccrs.PlateCarree(central_longitude = 0), draw_labels=True, linewidth = 1, color='gray', alpha=0.5, linestyle='--')   
-    gl.xlabels_top = gl.ylabels_right = False
-    ax.set_extent(extent, crs=ccrs.PlateCarree())
-
-    plt.pcolormesh(mrms.lon, mrms.lat, mrms.values, cmap = cmap.ref(), vmin = 0, vmax = 75, transform=ccrs.PlateCarree(central_longitude = 0))
-
-    t = str(data['time'][-1].values).split('T')
+    data = getData()
+    mrms = data['unknown'].sel(longitude = slice(extent[0] + 360, extent[1] + 360), latitude = slice(extent[3], extent[2]))
+    t = str(data['time'].values).split('T')
     time = f'{t[0]} at {t[1][0:5]}z'
 
-    plt.title(f'MRMS Base Reflectivity\n{time}', fontweight='bold', fontsize=10, loc='left')
-    plt.title(f'{title}\nDeelan Jariwala', fontsize=10, loc='right') 
-    
-    #print('Plotting complete!')
-    plt.colorbar(orientation = 'vertical', aspect = 50, pad = .02)
-    plt.savefig(r"C:\Users\deela\Downloads\mrmsradar.png", dpi = 250, bbox_inches = 'tight')
-    plt.show()
+    ax = map(.5, labelsize)
+    ax.set_extent(extent)
+    c = ax.pcolormesh(mrms.longitude, mrms.latitude, mrms.values, cmap = cmap.reflectivity(), vmin = -10, vmax = 70)
+    plt.title(f'MRMS Base Reflectivity\nTime: {time}' , fontweight='bold', fontsize=labelsize + 1, loc='left')
+    plt.title(f'{title}\nDeelan Jariwala', fontsize=labelsize + 1, loc='right')  
+    cbar = plt.colorbar(c, orientation = 'vertical', aspect = 50, pad = .02)
+    cbar.ax.tick_params(axis='both', labelsize=labelsize, left = False, bottom = False)
+    plt.savefig(r"C:\Users\deela\Downloads\mrmstest.png", dpi = 400, bbox_inches = 'tight')
     plt.close()
-plot(34.822, -98.757)
+    data.close()
+plot(30, -95)
